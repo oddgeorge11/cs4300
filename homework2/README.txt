2f854f6bc19c3da7b52250970ab9de2f784d212b
oddgeorge11 <mommykatgeo@gmail.com>
2026-03-06 06:51:43 +0000

    Added a README.txt as well as things to be able to deoply with Render

---
+++ homework2/README.txt
@@ -0,0 +1,58 @@
+Movei Theatre Booking app
+
+This is a Django app that allows a user to book from 5 different movie showings using Django REST Framework
+
+How to run locally:
+1) Install packages
+pip install -r requirements.txt
+
+2) Migrate database
+python manage.py migrate
+
+3) Run server
+python manage.py runserver 0.0.0.0:8000
+
+4) Open in browser
+http://localhost:8000/
+
+Additional endpoints:
+
+Login page:
+http://localhost:8000/accounts/login/
+
+Admin page:
+http://localhost:8000/admin/
+
+API endpoints:
+/api/movies/
+/api/seats/
+/api/bookings/
+
+To run on Render:
+./build.sh
+
+Start command
+python -m gunicorn movie_theater_booking.asgi:application -k uvicorn.workers.UvicornWorker
+
+Code Structure:
+homework2/
+  manage.py
+  requirements.txt
+  build.sh
+  movie_theater_booking/
+    settings.py
+    urls.py
+    wsgi.py
+    asgi.py
+  bookings/
+    models.py
+    views.py
+    urls.py
+    serializers.py
+    tests.py
+    templates/
+      bookings/
+      registration/
+    migrations/
+
+AI was used in tandom with the provided resources to help further explain how this project was to be accomplished and to help generate a starting point for each of the different 3.x assignment parts. For example, AI was used to help come up with some of the ideads for the Integration and BDD testing, as well as to review any code errors encounted to help explain issues and provide soltuions or reoucources to fix the issue.
--- homework2/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cwx@6)7nuc#*&@cj5vzpvq&v(#@+m(w8hio(yakt@&!zih-)35'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-cwx@6)7nuc#*&@cj5vzpvq&v(#@+m(w8hio(yakt@&!zih-)35"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["10.0.0.23", "localhost", "127.0.0.1", "0.0.0.0"]
+
+# Allow the Render hostname in production
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME and RENDER_EXTERNAL_HOSTNAME not in ALLOWED_HOSTS:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -43,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,10 +86,10 @@ WSGI_APPLICATION = 'movie_theater_booking.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
@@ -117,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_URL = "/accounts/login/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Login / logout configuration
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "movie_list"
 LOGOUT_REDIRECT_URL = "movie_list"
